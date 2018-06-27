34b44b5e0c57c488aca346729027ed4d14f0667e
gadgetgirlkim <kim.hansen@gmail.com>
2018-06-27 11:18:25 -0700

    Hello World folder added

---
+++ Assets/Scripts/CyllinderRotateScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyllinderRotateScript : MonoBehaviour
+{
+    public float speed = 1;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("DebugTest 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(0, 0, 1);
+        transform.Rotate(0, 0, speed);
+
+    }
+}
